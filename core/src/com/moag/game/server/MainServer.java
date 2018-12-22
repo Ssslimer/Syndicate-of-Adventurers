62f2896067d1ae61977b906fdcbd5d744a1a82be
Maciej Olejnik <maciek.olejnik@tlen.pl>
2018-12-22 23:51:59 +0100

    Removed wrong files, Added settings, sample icons and networking begin

---
+++ core/src/com/moag/game/server/MainServer.java
@@ -0,0 +1,13 @@
+package com.moag.game.server;
+
+
+public class MainServer
+{
+	public static void main(String[] args)
+	{
+        int port = 4444;
+
+        Server server = new Server(port); 
+       // server.start();
+	}
+}
--- desktop/src/com/moag/game/desktop/DesktopLauncher.java
+++ desktop/src/com/moag/game/desktop/DesktopLauncher.java
@@ -1,12 +1,23 @@
 package com.moag.game.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.moag.game.Game;
+import com.badlogic.gdx.graphics.Color;
+import com.moag.game.client.Game;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher
+{
+	public static void main(String[] arg)
+	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = true;
+		config.vSyncEnabled = true;
+		config.title = "Syndicate of Adventurers";
+		config.initialBackgroundColor = Color.BLACK;
+		config.addIcon("icon128x.png", FileType.Internal);
+		config.addIcon("icon32x.png", FileType.Internal);
+		config.addIcon("icon16x.png", FileType.Internal);
 		new LwjglApplication(new Game(), config);
 	}
 }
