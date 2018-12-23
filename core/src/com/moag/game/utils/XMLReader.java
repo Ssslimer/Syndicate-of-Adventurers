72dd42a20e389e6d3c7769a90caeb685bc6d8314
Adrian Gaj <fcb970@interia.pl>
2018-12-23 22:27:37 +0100

    Added XMLReader class for reading data from xml file;

---
+++ core/src/com/moag/game/utils/XMLReader.java
@@ -0,0 +1,57 @@
+package com.moag.game.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+public class XMLReader 
+{
+	private final String xmlFileString = "server_properties.xml";
+	
+	private DocumentBuilderFactory docBuilderFactory;
+	private DocumentBuilder docBuilder;
+	private Document doc;
+	
+	private Element server_info;
+	
+	public XMLReader()
+	{	
+		try 
+		{
+			docBuilderFactory = DocumentBuilderFactory.newInstance();
+			docBuilder = docBuilderFactory.newDocumentBuilder();
+			doc = docBuilder.parse(new File(xmlFileString));
+			
+			doc.getDocumentElement().normalize();
+			
+			server_info = doc.getElementById("1001");
+			
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+			
+		} catch (SAXException e) {
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String getIP()
+	{		
+		return server_info.getElementsByTagName("ip").item(0).getTextContent();
+	}
+	
+	public int getPortNumber()
+	{
+		return Integer.parseInt(server_info.getElementsByTagName("port").item(0).getTextContent());
+	}
+}
