fcbe23b0ae7aaf5b105522689f3352163f2b1656
Adrian Gaj <fcb970@interia.pl>
2018-12-29 16:07:12 +0100

    Started Entity player implementation, added Item class.

---
+++ core/src/com/moag/game/entities/EntityPlayer.java
@@ -0,0 +1,96 @@
+package com.moag.game.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.math.Vector3;
+
+public class EntityPlayer extends Entity
+{
+	private static final int BASE_ATTACK = 10;
+	private static final int BASE_DEFENSE = 5;
+	private static final int BASE_HP = 100;
+	
+	private long playerID; // equal to sessionID ?
+	
+	private int HP;
+	private int attackPower;
+	private int defencePower;
+	
+	private List<Item> eqList = new ArrayList<>(); 
+	
+	public EntityPlayer(Vector3 position) {
+		super(position);
+		
+		HP = BASE_HP;
+		attackPower = BASE_ATTACK;
+		defencePower = BASE_DEFENSE;
+	}
+	
+	public EntityPlayer(EntityPlayer player)
+	{
+		super(player.getPosition());
+		
+		playerID = player.getPlayerID();
+		
+		eqList = player.getEqList();
+		determinePlayerStats();
+	}
+	
+	public long getPlayerID()
+	{
+		return playerID;
+	}
+	
+	public int getHP()
+	{
+		return HP;
+	}
+	
+	public int getAttackPower()
+	{
+		return attackPower;
+	}
+	
+	public int getDefencePower()
+	{
+		return defencePower;
+	}
+	
+	public List<Item> getEqList()
+	{
+		return eqList;
+	}
+	
+	public void addItem(Item item)
+	{
+		eqList.add(item);
+		
+		HP += item.getHPBonus();
+		attackPower += item.getAttack();
+		defencePower += item.getDefence();
+	}
+	
+	public void removeItem(Item item)
+	{
+		if(eqList.contains(item))
+		{
+			eqList.remove(item);
+			
+			HP -= item.getHPBonus();
+			attackPower -= item.getAttack();
+			defencePower -= item.getDefence();
+		}
+	}
+	
+	private void determinePlayerStats()
+	{
+		for(Item item : eqList)
+		{
+			HP += item.getHPBonus();
+			attackPower += item.getAttack();
+			defencePower += item.getDefence();
+		}
+	}
+
+}
